03f65fae0796827b488c09146f3f7dbf046180d0
Britney Hawthorne <britneyhawthorne@Britneys-MacBook-Air.local>
2025-10-10 14:53:22 -0500

    Exercise 1

---
+++ tic-tac-toe.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const squares = document.querySelectorAll('#board div');
+    squares.forEach(function(square) {
+        square.classList.add('square');
+    });
+    console.log('Squares startup');
+});
