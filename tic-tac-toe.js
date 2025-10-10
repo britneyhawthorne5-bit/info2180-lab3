e3175e6a1028cd309f229931657ece56f37fefed
Britney Hawthorne <britneyhawthorne@Britneys-MacBook-Air.local>
2025-10-10 15:53:05 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     const squares = document.querySelectorAll('#board div');
-    squares.forEach(function(square) {
+    let nextIs = true; 
+    const boardState = [null, null, null, null, null, null, null, null, null];
+
+    squares.forEach(function(square, index) {
         square.classList.add('square');
+
+        square.addEventListener('click', function() {
+            if (boardState[index] == 'O' || boardState[index] == 'X') return;
+            let mark = null;
+            if (nextIs) {
+                mark = 'X';
+            } else {
+                mark = 'O';
+            }
+            square.textContent = mark;          
+            square.classList.add(mark);        
+            boardState[index] = mark;           
+
+            nextIs = !nextIs;                 
+        });
     });
+
     console.log('Squares startup');
 });
+
